e85a193ff4a63ed01ebd2c79dc3fecae82b79ea1
Sander Blue <william.a.blue@gmail.com>
2017-04-03 14:21:54 -0400

    getting the chart to render

---
+++ src/components/LineChart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="Chart__list">
+      <div class="Chart">
+        <h2>Linechart</h2>
+        <line-example></line-example>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import LineExample from './components/LineChart.js';
+
+export default {
+  name: 'app',
+  components: {
+    LineExample,
+  },
+};
+</script>
+
+
+
+
+<style scoped>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
