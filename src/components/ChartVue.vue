a5cbb12b0ffe46e9c0bacf661be3b78a4082ebb9
Sander Blue <william.a.blue@gmail.com>
2017-05-08 11:42:20 -0500

    things are starting to move along quite nicely now

--- src/components/ChartVue.vue
+++ src/components/ChartVue.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="chart-container">
-    <label for="zip">Zip Code</label>
-    <input id="zip" type="text" name="zip">
+    <div class="row collapse">
+      <div class="small-12 columns">
+        <label for="zip">Zip Code</label>
+        <input id="zip" type="text" name="zip" v-model="zip">
+      </div>
+    </div>
     <canvas class="chart"></canvas>
   </div>
 </template>
@@ -9,9 +13,17 @@
 
 <script>
 import Chart from 'chart.js'
+import axios from 'axios'
+import _ from 'lodash'
+import moment from 'moment'
+import GeolocationModel from '../app/GeolocationModel'
+
+const red = 'rgba(254, 74, 73, 0.9)';
+const blue = 'rgba(42, 183, 202, 0.9)';
 
 console.clear()
 console.log('')
+// console.debug('GeolocationModel', GeolocationModel)
 
 /* eslint-disable no-new */
 export default {
@@ -19,32 +31,133 @@ export default {
 
   data() {
     return {
+      zip: 'test',
+      forecastData: [],
+      labels: [],
+      datasets: [],
+    }
+  },
 
+  watch: {
+    chartData: function () {
+
+    },
+
+    forecastData: function (forecast) {
+      console.debug('Watch forecastData...', forecast);
+
+      let dates = this.extractDates(forecast);
+      let temps = this.extractTemps(forecast);
+
+      // console.debug('Dates:', dates);
+      // console.debug('Temps:', temps);
+      // console.debug('Highs:', _.map(temps, 'high'));
+      // console.debug('Lows: ', _.map(temps, 'low'));
+
+      let datesets = [
+        {
+          label: 'Highs',
+          fill: false,
+          data: _.map(temps, 'high'),
+          borderColor: red
+        },
+        {
+          label: 'Lows',
+          fill: false,
+          data: _.map(temps, 'low'),
+          borderColor: blue
+        },
+      ];
+
+      this.setLabels(dates);
+      this.setDatasets(datesets);
+      this.renderChart(true);
+    }
+  },
+
+  methods: {
+    extractDates(arr) {
+      return _.map(arr, function (dayData, index) {
+        let date = moment.unix(dayData.date.epoch);
+
+        return date.format('ddd MMM D');
+      });
+    },
+
+    extractTemps(arr) {
+      return _.map(arr, function (dayData, index) {
+        return {
+          high: dayData.high.fahrenheit,
+          low: dayData.low.fahrenheit
+        };
+      });
+    },
+
+    setDatasets(datasets) {
+      this._chart.data.datasets = datasets;
+    },
+
+    setLabels(labels) {
+      this._chart.data.labels = labels;
+    },
+
+    renderChart: function (update) {
+      console.debug('Rendering...', this._chart.data.datasets);
+
+      if (update) {
+        this._chart.update();
+      } else {
+        this._chart.render();
+      }
     }
   },
 
   beforeCreate() {
-    console.debug('beforeCreate...', this);
+    this.geolocationModel = new GeolocationModel();
+
+    this.geolocationModel.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    });
+
+
+    // console.debug('beforeCreate...', this);
+
+    let tenDayForecastUrl = 'http://api.wunderground.com/api/1e0a7bd45ab35633/forecast10day/q/TN/Nashville.json';
+
+    let currentConditionsUrl = 'http://api.wunderground.com/api/1e0a7bd45ab35633/conditions/q/CA/San_Francisco.json';
+
+    // api.wunderground.com/api/1e0a7bd45ab35633/history_'+ date +'/q/81657.json
+
+    // let request = new Request(endpoint, {
+    //   method: 'get',
+    // });
+
+    axios.get(tenDayForecastUrl).then((res) => {
+      let forecastData = res.data.forecast.simpleforecast.forecastday;
+
+      console.debug('Forecast...', forecastData[0]);
+
+      this.forecastData = forecastData;
+    })
   },
 
   created: function () {
-    console.debug('created...     ', this);
+    // console.debug('created...     ', this);
   },
 
   mounted() {
-    console.debug('mounted...     ', this)
+    console.debug('mounted...     ', this.datasets)
 
-    let canvasElement = this.$el.querySelector('canvas');
+    this.canvasElement = this.$el.querySelector('canvas')
 
     // init chart.js
-    let chart = new Chart(canvasElement, {
+    this._chart = new Chart(this.canvasElement, {
       type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3]
-        }]
+        labels: this.labels,
+        datasets: this.datasets
       },
       options: {
         responsive: true,
@@ -59,7 +172,7 @@ export default {
       }
     })
 
-    chart.render();
+    this.renderChart(false);
   }
 }
 </script>
