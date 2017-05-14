bedfbb721108406daf00c90e3d216ddbcaca4c3d
Sander Blue <william.a.blue@gmail.com>
2017-05-14 18:43:22 -0500

    use localStorage to minimize wunderground API rate limit issues

--- src/components/CurrentConditions.vue
+++ src/components/CurrentConditions.vue
@@ -20,7 +20,7 @@
     </div>
     <div class="row hourly-details">
       <div class="small-12 columns">
-        <h4 class="text-center">Hourly Forecast</h4>
+        <h4 class="text-center">12-hour Forecast</h4>
         <div class="chart-container">
           <canvas class="chart"></canvas>
         </div>
@@ -119,115 +119,168 @@ export default {
 
   watch: {
     geoCoordinates(value) {
-
-      let hourlyUrl = `${apiPrefix}/hourly/q/${value.latitude},${value.longitude}.json`;
-
-      axios.get(hourlyUrl).then((res) => {
-        let data = res.data.hourly_forecast;
-
-        let mappedData = _.map(data, (hourlyData) => {
-          // console.debug('value', hourlyData);
-
-          return {
-            epoch: hourlyData.FCTTIME.epoch,
-            temp: {
-              f: hourlyData.temp.english,
-              c: hourlyData.temp.metric
-            },
-            relativeHumidity: hourlyData.humidity
-          };
-        });
-
-        this.forecast.hourly = mappedData;
-        this.chartLabels = this.extractDates(this.forecast.hourly);
-
-        let temps = _.map(this.forecast.hourly, 'temp.f');
-        temps.length = 12;
-
-        this.chartDatasets = [
-          {
-            label: 'Temp',
-            fill: false,
-            data: temps,
-            borderColor: red
-          }
-        ];
-
-        // console.debug('extractDates', this.extractDates(this.forecast.hourly));
-
-        let hourlyTimes = this.extractDates(this.forecast.hourly);
-
-        console.debug('this.chartDatasets', hourlyTimes.length);
-
-        hourlyTimes.length = 12;
-
-        this.setLabels(hourlyTimes);
-        this.setDatasets(this.chartDatasets);
-        this.renderChart(true);
-
-        // console.debug('Hourly data...', this.forecast.hourly);
-      });
-
       let currentConditionsUrl = `${apiPrefix}/conditions/q/${value.latitude},${value.longitude}.json`;
 
-      axios.get(currentConditionsUrl).then((res) => {
-        let data = res.data.current_observation;
+      let key = 'wx';
+      let wxData = this.getData(key);
 
-        console.debug('Current Conditions...', data);
+      if (!wxData) {
+        this.getWxData(currentConditionsUrl)
+          .then(this.setData.bind(this))
+          .then(() => {
+            console.debug('No data to start, but data has been fetched and set now!');
+          });
 
-        this.icon = weatherIconMap[data.icon_url];
+      } else {
+        let savedWxData = JSON.parse(wxData);
+        let epochNow = moment().unix();
+        let elapsedMinutes = this.getElapsedMinutes(epochNow, savedWxData.checkedAt.epoch);
 
-        this.wx = {
-          station: {
-            city: data.observation_location.city,
-            state: data.observation_location.state,
-            country: data.observation_location.country,
-            full: data.observation_location.full,
-            elevation: data.observation_location.elevation,
-            latitude: data.observation_location.latitude,
-            longitude: data.observation_location.latitude,
-          },
-          location: {
-            city: data.display_location.city,
-            state: data.display_location.state,
-            country: data.display_location.country,
-            elevation: data.display_location.elevation,
-            fullName: data.display_location.full,
-            latitude: data.display_location.latitude,
-            longitude: data.display_location.longitude,
-            zip: data.display_location.zip,
-          },
-          updatedAt: {
-            epoch: data.observation_epoch,
-            date: new Date(data.observation_epoch * 1000),
-            timestamp: data.observation_epoch * 1000
-          },
-          weather: data.weather,
-          temp: {
-            c: data.temp_c,
-            f: data.temp_f,
-            k: data.temp_c + 273.15 // Kelvin
-          },
-          dewpoint: {
-            c: data.dewpoint_c,
-            f: data.dewpoint_f
-          },
-          relativeHumidity: data.relative_humidity,
-          heatIndex: {
-            c: data.heat_index_c,
-            f: data.heat_index_f
-          },
-          uv: data.uv,
-          icon: data.icon,
-          wxIcon: weatherIconMap[data.icon]
-        };
+        // Only get new weather data every 10 minutes
+        if (elapsedMinutes >= 10) {
+          console.debug('Fetching new weather data...');
 
-        console.debug('this.wx:', this.wx.icon);
-      });
+          this.getWxData(currentConditionsUrl)
+            .then(this.setData.bind(this))
+            .then(() => {
+              console.debug('Data has been set!');
+            });
+        } else {
+          this.wx = JSON.parse(wxData);
+        }
+      }
+
+      // let hourlyUrl = `${apiPrefix}/hourly/q/${value.latitude},${value.longitude}.json`;
+
+      // axios.get(hourlyUrl).then((res) => {
+      //   let data = res.data.hourly_forecast;
+
+      //   let mappedData = _.map(data, (hourlyData) => {
+      //     // console.debug('value', hourlyData);
+
+      //     return {
+      //       epoch: hourlyData.FCTTIME.epoch,
+      //       temp: {
+      //         f: hourlyData.temp.english,
+      //         c: hourlyData.temp.metric
+      //       },
+      //       relativeHumidity: hourlyData.humidity
+      //     };
+      //   });
+
+      //   this.forecast.hourly = mappedData;
+      //   this.chartLabels = this.extractDates(this.forecast.hourly);
+
+      //   let temps = _.map(this.forecast.hourly, 'temp.f');
+      //   temps.length = 12;
+
+      //   this.chartDatasets = [
+      //     {
+      //       label: 'Temp',
+      //       fill: false,
+      //       data: temps,
+      //       borderColor: red,
+      //       pointRadius: 0.5,
+      //       pointHitRadius: 5,
+      //       pointHoverBackgroundColor: red,
+      //       pointHoverBorderColor: red
+      //     }
+      //   ];
+
+      //   // console.debug('extractDates', this.extractDates(this.forecast.hourly));
+
+      //   let hourlyTimes = this.extractDates(this.forecast.hourly);
+
+      //   console.debug('this.chartDatasets', hourlyTimes.length);
+
+      //   hourlyTimes.length = 12;
+
+      //   this.setLabels(hourlyTimes);
+      //   this.setDatasets(this.chartDatasets);
+      //   this.renderChart(true);
+
+      //   // console.debug('Hourly data...', this.forecast.hourly);
+      // });
     }
   },
 
   methods: {
+    getElapsedMinutes(epochNow, epochBefore) {
+      let elapsedSeconds = epochNow - epochBefore;
+
+      return elapsedSeconds / 60;
+    },
+
+    getWxData(url) {
+      return axios.get(url);
+    },
+
+    setData(res) {
+      console.debug('Setting data...', res);
+
+      let data = res.data.current_observation;
+
+      this.icon = weatherIconMap[data.icon_url];
+
+      this.wx = {
+        station: {
+          city: data.observation_location.city,
+          state: data.observation_location.state,
+          country: data.observation_location.country,
+          full: data.observation_location.full,
+          elevation: data.observation_location.elevation,
+          latitude: data.observation_location.latitude,
+          longitude: data.observation_location.latitude,
+        },
+        location: {
+          city: data.display_location.city,
+          state: data.display_location.state,
+          country: data.display_location.country,
+          elevation: data.display_location.elevation,
+          fullName: data.display_location.full,
+          latitude: data.display_location.latitude,
+          longitude: data.display_location.longitude,
+          zip: data.display_location.zip,
+        },
+        updatedAt: {
+          epoch: data.observation_epoch,
+          date: new Date(data.observation_epoch * 1000),
+          timestamp: data.observation_epoch * 1000
+        },
+        checkedAt: {
+          epoch: moment().unix()
+        },
+        weather: data.weather,
+        temp: {
+          c: data.temp_c,
+          f: data.temp_f,
+          k: data.temp_c + 273.15 // Kelvin
+        },
+        dewpoint: {
+          c: data.dewpoint_c,
+          f: data.dewpoint_f
+        },
+        relativeHumidity: data.relative_humidity,
+        heatIndex: {
+          c: data.heat_index_c,
+          f: data.heat_index_f
+        },
+        uv: data.uv,
+        icon: data.icon,
+        wxIcon: weatherIconMap[data.icon]
+      };
+
+      this.storeData('wx', this.wx);
+    },
+
+    storeData(key, data) {
+      localStorage.setItem(key, JSON.stringify(data));
+    },
+
+    getData(key) {
+      return localStorage.getItem(key);
+    },
+
     extractDates(arr) {
       return _.map(arr, (data, index) => {
         let date = moment.unix(data.epoch);
@@ -282,15 +335,12 @@ export default {
   },
 
   mounted() {
-    // console.debug('mounted...')
-
     this.canvasElement = this.$el.querySelector('canvas');
 
     Chart.defaults.global.legend = {
       display: false
     };
 
-    // init chart.js
     this.chartHourly = new Chart(this.canvasElement, {
       type: 'line',
       data: {
@@ -303,7 +353,35 @@ export default {
         scales: {
           yAxes: [{
             ticks: {
-              beginAtZero: true
+              beginAtZero: true,
+              padding: 5,
+              // mirror: true
+            },
+            gridLines: {
+              drawTicks: false
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              // labelOffset: 10,
+              padding: 0,
+              mirror: false,
+              maxRotation: 0,
+              fontSize: 10,
+              callback: function (value, index, values) {
+                console.debug('Tick...', index, values.length - 1);
+
+                if (index === 0) {
+                  return;
+                }
+
+                if (index % 2 === 0) {
+                  return value;
+                }
+              }
+            },
+            gridLines: {
+              drawTicks: false
             }
           }]
         }
@@ -357,7 +435,8 @@ export default {
 }
 
 .chart-container {
-  height: 25vh;
+  height: 20vh;
+  max-height: 120px;
   width: 90vw;
 
   canvas {
