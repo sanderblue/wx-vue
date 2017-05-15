490c5208c88885f7bf775ba263954714dd5a2fe8
Sander Blue <william.a.blue@gmail.com>
2017-05-14 19:06:30 -0500

    move search form to its own component to start handling this functionality

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="main-content">
-    <currentconditions></currentconditions>
+  <div>
+
+    <header class="row header">
+      <div class="small-3 wx-title">WxVue</div>
+      <div class="small-6">
+        <search></search>
+      </div>
+      <div class="small-3 text-right">
+        <button class="menu-icon" type="button" data-open="wx-menu"></button>
+      </div>
+    </header>
+
+    <div class="off-canvas position-right" id="wx-menu" data-off-canvas>
+      <ul class="menu vertical">
+        <li><a href="#">Radar</a></li>
+        <li><a href="#">History Data</a></li>
+      </ul>
+    </div>
+
+    <main class="row small-12 columns small-centered" role="main">
+      <div class="main-content">
+        <currentconditions></currentconditions>
+      </div>
+    </main>
+
+    <footer></footer>
   </div>
 </template>
 
 
 <script>
-// import Chart from './components/ChartVue';
+import Search from './components/Search';
 import CurrentConditions from './components/CurrentConditions';
 
 export default {
   name: 'app',
   components: {
-    // 'chart': Chart,
+    'search': Search,
     'currentconditions': CurrentConditions
   },
   mounted() {
